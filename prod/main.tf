e21928986e519c22841544282f56d63b2095b83f
Raphael Okere <raphaelokere@Raphaels-MBP.attlocal.net>
2024-09-12 00:20:04 -0400

    more tests

--- prod/main.tf
+++ prod/main.tf
@@ -6,21 +6,19 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "<your-bucket-name>"
-    key            = "terraform-prod.tfstate"
-    region         = "<bucket-region>"
-    # Optional DynamoDB for state locking. See https://developer.hashicorp.com/terraform/language/settings/backends/s3 for details.
-    # dynamodb_table = "terraform-state-lock-table"
-    encrypt        = true
-    role_arn       = "arn:aws:iam::<your-aws-account-no>:role/<terraform-s3-backend-access-role>"
-  }
+  # backend "s3" {
+  #   bucket         = "snowflake-raphaelokere"
+  #   key            = "terraform-prod.tfstate"
+  #   region         = "us-east-1"
+  #   encrypt        = true
+  #   role_arn       = "arn:aws:iam::658108419956:role/snowflake"
+  # }
 }
 
 provider "snowflake" {
-  username    = "<your_snowflake_username>"
-  account     = "<your_snowflake_account_identifier>"
-  role        = "<your_snowflake_role>"
+  username    = "RAPHAELOKERE"
+  account     = "YQB16430.us-east-1"
+  role        = "ACCOUNTADMIN"
   private_key = var.snowflake_private_key
 }
 
